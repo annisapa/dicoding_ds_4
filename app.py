8fd9453111528dcd8a7d1d528fa706e6dc01b0b6
Annisa Permatasari Ayuningtyas <annisapermatasaria@gmail.com>
2023-10-20 20:00:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Capital Bike Sharing Dashboard", page_icon=":bar_chart:", layout="wide")
 
